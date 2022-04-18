724e084047929396ff19fbfd542c10813b94eeda
Philipp Khromov <filooloog@mail.ru>
2022-04-18 17:27:19 +0300

    fix: fully implement burger-menu functionality

--- shelter/scripts/script.js
+++ shelter/scripts/script.js
@@ -4,6 +4,7 @@ const header = document.querySelector('.header__container');
 const headerMenu = document.querySelector('.header__menu');
 const burgerButton = document.querySelector('.burger__button');
 const overlay = document.querySelector('.overlay');
+const menuList = document.querySelector('.menu__list');
 
 burgerButton.addEventListener('click', ()=>{
   headerMenu.classList.toggle('menu--active');
@@ -17,3 +18,19 @@ burgerButton.addEventListener('click', ()=>{
   }
 
 })
+
+overlay.addEventListener('click', (e)=> {
+  headerMenu.classList.remove('menu--active');
+  burgerButton.classList.remove('menu--active');
+  overlay.classList.remove('overlay__active');
+  body.style.cssText = 'overflow-y: visible';
+})
+
+menuList.addEventListener('click', ()=> {
+  headerMenu.classList.remove('menu--active');
+  burgerButton.classList.remove('menu--active');
+  overlay.classList.remove('overlay__active');
+  body.style.cssText = 'overflow-y: visible';
+})
+
+body.addEventListener('click', (e)=>console.log(e.target))
