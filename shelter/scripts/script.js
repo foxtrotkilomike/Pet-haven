51f9f32da856418401dd5274e9cc64b2c56c9c88
Philipp Khromov <filooloog@mail.ru>
2022-04-24 18:21:16 +0300

    feat: implement burger according to task

--- shelter/scripts/script.js
+++ shelter/scripts/script.js
@@ -1,36 +1,54 @@
+import petsData from '../assets/pets.json' assert {type: "json"};
+
+console.log(petsData)
+
 const body = document.querySelector('body')
 const wrapper = document.querySelector('.wrapper');
-const header = document.querySelector('.header__container');
+const header = document.querySelector('.header');
+const headerContainer = document.querySelector('.header__container');
 const headerMenu = document.querySelector('.header__menu');
 const burgerButton = document.querySelector('.burger__button');
+
 const overlay = document.querySelector('.overlay');
+
 const menuList = document.querySelector('.menu__list');
+
+
+
+const createElement = (tagName, tagClass, textContent) => {
+  const element = document.createElement(tagName);
+  element.classList.add(tagClass);
+  if (textContent) {
+    element.textContent = textContent;
+  }
+
+  return element;
+}
+
+const hideMenu = () => {
+  headerMenu.classList.remove('menu--active');
+  burgerButton.classList.remove('menu--active');
+  overlay.classList.remove('overlay__active');
+  body.classList.remove('noscroll');
+}
 
 burgerButton.addEventListener('click', ()=>{
   headerMenu.classList.toggle('menu--active');
   burgerButton.classList.toggle('menu--active');
-  if (document.querySelector('.overlay__active')) {
-    overlay.classList.remove('overlay__active');
-    body.style.cssText = 'overflow-y: visible';
-  } else {
+  if (headerContainer.querySelector('.menu--active')) {
     overlay.classList.add('overlay__active');
-    body.style.cssText = 'overflow-y: hidden;';
+    overlay.addEventListener('click', () => hideMenu() )
+    body.classList.add('noscroll')
+  } else {
+    overlay.classList.remove('overlay__active');
+    overlay.removeEventListener('click', () => hideMenu() )
+    body.classList.remove('noscroll')
   }
 
 })
 
-overlay.addEventListener('click', (e)=> {
-  headerMenu.classList.remove('menu--active');
-  burgerButton.classList.remove('menu--active');
-  overlay.classList.remove('overlay__active');
-  body.style.cssText = 'overflow-y: visible';
-})
-
 menuList.addEventListener('click', ()=> {
-  headerMenu.classList.remove('menu--active');
-  burgerButton.classList.remove('menu--active');
-  overlay.classList.remove('overlay__active');
-  body.style.cssText = 'overflow-y: visible';
+  hideMenu();
 })
 
 body.addEventListener('click', (e)=>console.log(e.target))
