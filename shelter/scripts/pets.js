e2560fad9180de23c937ab682563618bac822888
Philipp Khromov <filooloog@mail.ru>
2022-04-26 13:13:58 +0300

    feat: implement pagination on pets page

--- shelter/scripts/pets.js
+++ shelter/scripts/pets.js
@@ -1,21 +1,182 @@
 import petsData from '../assets/pets.json' assert { type: "json" };
 import * as burger from './burger.js'
-import { getUniquePetId} from './pet3Factory.js'
+
+const cardsContainer = document.querySelector('.cards__container')
+const petTemplate = document.querySelector('#pet-template').content;
+const petCard = petTemplate.querySelector('.pet-card');
+const firstPageBtn = document.getElementById('first-page');
+const prevPageBtn = document.getElementById('prev-page');
+const currPageBtn = document.getElementById('curr-page');
+const nextPageBtn = document.getElementById('next-page');
+const lastPageBtn = document.getElementById('last-page');
+
+const allPetsArray = [];
+for (let i = 0; i < 6; i++) {
+  for (let j = 0; j < 8; j++) {
+    allPetsArray.push(j);
+  }
+}
+
+const shuffle = (arrayInit) => {
+  const array = arrayInit.slice();
+  let currentIndex = array.length,  randomIndex;
+
+  while (currentIndex != 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+    [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+}
+
+const make6By8 = () => {
+  const subArray = [0, 1, 2, 3, 4, 5, 6, 7];
+  const set6By8 = [];
+  // generate 6 sets (pages)
+  for (let i = 0; i < 6; i++) {
+    set6By8.push(shuffle(subArray));
+  }
+
+  return set6By8;
+}
+
+const make8By6 = () => {
+  let currentIndex = 0;
+  const set8By6 = [];
+  let subArray = [];
+  // fill the matrix
+  for (let i = 0; i < 8; i++) {
+    for (let j = 0; j < 6; j++) {
+      subArray.push(allPetsArray[currentIndex])
+      currentIndex++;
+    }
+    set8By6.push(shuffle(subArray));
+    subArray = [];
+  }
+  return set8By6;
+}
+
+const make16By3 = () => {
+  let currentIndex = 0;
+  const set16By3 = [];
+  let subArray = [];
+  // fill the matrix
+  for (let i = 0; i < 16; i++) {
+    for (let j = 0; j < 3; j++) {
+      subArray.push(allPetsArray[currentIndex])
+      currentIndex++;
+    }
+    set16By3.push(shuffle(subArray));
+    subArray = [];
+  }
+  return set16By3;
+}
+
+
+
+let petInstances = [];
 
 const pet8CardFactory = () => {
-  petsIdOld = petsIdNew;
-  petsIdNew = [];
-  let newId;
-  for (let i = 0; i < 3; i++) {
-    newId = getUniquePetId(petsIdOld, petsIdNew)
-    petsIdNew.push(newId);
+  for (let i = 0; i < 8; i++) {
     const petInstance = petCard.cloneNode(true);
-    petInstance.children[0].src = `../${petsData[newId].img}`;
-    petInstance.children[0].alt = petsData[newId].type;
-    petInstance.children[1].textContent = petsData[newId].name;
-    console.log(petInstance)
-    // sliderContainer.append(petInstance);
+    petInstance.children[0].src = `../${petsData[i].img}`;
+    petInstance.children[0].alt = petsData[i].type;
+    petInstance.children[1].textContent = petsData[i].name;
+    petInstances.push(petInstance)
   }
 }
 
 pet8CardFactory();
+
+let currentPage = 1;
+let lastPageNumber;
+const set6By8 = make6By8();
+const set8By6 = make8By6();
+const set16By3 = make16By3();
+
+const makePage = (pageNumber) => {
+  if (window.matchMedia("(min-width: 1280px)").matches) {
+    set6By8[pageNumber].forEach((petIndex) => {
+      let card = petInstances[petIndex].cloneNode(true);
+      cardsContainer.append(card)
+    })
+    lastPageNumber = 6;
+  }
+
+  if (window.matchMedia("(min-width: 768px) and (max-width: 1279px)").matches) {
+    set8By6[pageNumber].forEach((petIndex) => {
+      let card = petInstances[petIndex].cloneNode(true);
+      cardsContainer.append(card)
+    })
+    lastPageNumber = 8;
+  }
+
+  if (window.matchMedia("(min-width: 320px) and (max-width: 767px)").matches) {
+    set16By3[pageNumber].forEach((petIndex) => {
+      let card = petInstances[petIndex].cloneNode(true);
+      cardsContainer.append(card)
+    })
+    lastPageNumber = 16;
+  }
+}
+
+makePage(0);
+
+firstPageBtn.addEventListener('click', () => {
+  currentPage = 1;
+  cardsContainer.innerHTML = '';
+  makePage(currentPage - 1);
+  firstPageBtn.classList.add('button--disabled');
+  prevPageBtn.classList.add('button--disabled');
+  nextPageBtn.classList.remove('button--disabled');
+  lastPageBtn.classList.remove('button--disabled');
+  currPageBtn.textContent = currentPage;
+});
+
+lastPageBtn.addEventListener('click', () => {
+  currentPage = lastPageNumber;
+  cardsContainer.innerHTML = '';
+  makePage(currentPage - 1);
+  nextPageBtn.classList.add('button--disabled');
+  lastPageBtn.classList.add('button--disabled');
+  firstPageBtn.classList.remove('button--disabled');
+  prevPageBtn.classList.remove('button--disabled');
+  currPageBtn.textContent = currentPage;
+});
+
+prevPageBtn.addEventListener('click', () => {
+  if (currentPage > 1) {
+    currentPage--;
+    cardsContainer.innerHTML = '';
+    makePage(currentPage - 1);
+    if (currentPage === 1) {
+      firstPageBtn.classList.add('button--disabled');
+      prevPageBtn.classList.add('button--disabled');
+    }
+
+    if (currentPage === lastPageNumber - 1) {
+      nextPageBtn.classList.remove('button--disabled');
+      lastPageBtn.classList.remove('button--disabled');
+    }
+    currPageBtn.textContent = currentPage;
+  }
+});
+
+nextPageBtn.addEventListener('click', () => {
+  if (currentPage < lastPageNumber) {
+    currentPage++;
+    cardsContainer.innerHTML = '';
+    makePage(currentPage - 1);
+    if (currentPage === lastPageNumber) {
+      nextPageBtn.classList.add('button--disabled');
+      lastPageBtn.classList.add('button--disabled');
+    }
+
+    if (currentPage === 2) {
+      firstPageBtn.classList.remove('button--disabled');
+      prevPageBtn.classList.remove('button--disabled');
+    }
+    currPageBtn.textContent = currentPage;
+  }
+});
